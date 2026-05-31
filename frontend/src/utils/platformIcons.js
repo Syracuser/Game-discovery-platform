9eebad0af6a38aa57c55af3302462f34408e0da2
Shahak <90shml@gmail.com>
2026-05-31 20:35:16 +0300

    added IconMapper utility

---
+++ frontend/src/utils/platformIcons.js
@@ -0,0 +1,11 @@
+import { FaWindows, FaPlaystation, FaXbox } from "react-icons/fa";
+import { SiNintendoswitch } from "react-icons/si";
+
+const PLATFORM_ICONS = {
+  "PC": FaWindows,
+  "PlayStation": FaPlaystation,
+  "Xbox": FaXbox,
+  "Nintendo Switch": SiNintendoswitch,
+};
+
+export default PLATFORM_ICONS;
